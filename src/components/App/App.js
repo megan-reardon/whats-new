6c2ff41f6f23fdb3bc044587fa6ed6698029c9ae
Megan Venetianer <megan.venetianer@gmail.com>
2020-03-17 21:29:05 -0600

    filter on article type

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import NewsArticle from '../NewsArticle/NewsArticle';
+import Menu from '../Menu/Menu';
 import local from '../../data/local';
 import entertainment from '../../data/entertainment';
 import health from '../../data/health';
@@ -12,14 +13,37 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      news: {
+        local: local,
+        entertainment: entertainment,
+        health: health,
+        science: science,
+        technology: technology,
+        selectCategory: 'technology'
+      }
     }
+  };
+
+  filterCategory = (event) => {
+      this.setState({
+        news: {
+          local: local,
+          entertainment: entertainment,
+          health: health,
+          science: science,
+          technology: technology,
+          selectCategory: [event.target.id]
+        }
+      })
   }
 
   render () {
+    {console.log(this.state.news[this.state.news.selectCategory])}
     return (
       <div className="app">
-        <NewsContainer news={this.state.local}/>
+        <Menu filterCategory={this.filterCategory}/>
+        <NewsContainer
+         news={this.state.news[this.state.news.selectCategory]}/>
       </div>
     );
   }
